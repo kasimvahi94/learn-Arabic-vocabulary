b89be1d4d8866df398c95813c883b08d3d570d03
kasimvahi94 <kasimvahi94@gmail.com>
2026-02-21 15:20:58 +0530

    Add files via upload

---
+++ src_vocab_Version2.js
@@ -0,0 +1,12 @@
+export const quranicVocab = [
+  { arabic: "رَبّ", translation: "Lord", example: "رَبِّ الْعَالَمِينَ" },
+  { arabic: "مَلِك", translation: "King", example: "مَلِكِ النَّاسِ" },
+  { arabic: "يَوْم", translation: "Day", example: "يَوْمِ الدِّينِ" },
+  { arabic: "هُوَ", translation: "He", example: "هُوَ اللَّهُ أَحَدٌ" },
+];
+export const otherVocab = [
+  { arabic: "كِتَاب", translation: "Book", example: "هَذَا كِتَابٌ" },
+  { arabic: "سَيَّارَة", translation: "Car", example: "السَيَّارَةُ جَدِيدَةٌ" },
+  { arabic: "مَدرَسَة", translation: "School", example: "الْمَدْرَسَةُ كَبِيرَةٌ" },
+  { arabic: "مَاء", translation: "Water", example: "أُرِيدُ مَاءً" },
+];
